4eac6b11823bf0c4984fe766295f39d8f81a31fb
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-06-14 18:31:55 -0300

    made some changes to the js

--- assets/script.js
+++ assets/script.js
@@ -1,88 +1,77 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(document).ready(function () {
-    $('saveBtn').on("click"),
-      function () {
+  $(".saveBtn").on("click", function () {
         console.log(this);
-        var value = $(this).siblings('.description')
-        var time = $(this).attr('time-value')
-        var hour = timeSlot().hour();
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr('id');
+        console.log(time);
+       // var hour = .;
 
         localStorage.setItem(time, value);
-      };
-
-  });
-
-
- var currentTime = dayjs().format('dddd MMMM DD')
-    $('#currentDay').text(currentTime);
-
-    function displayCurrentDate(){
-
-
-    }
-
-    var scheduleARR = [];
-
-    for (var i = 9; i < 18; i++) {
-      scheduleARR.push(timeSlot().hour(i).format('h,a'));
-    }
-    console.log(scheduleARR);
-
-
-    function setColors() {
-      var currentTime = moment().hours();
-      console.log(currentTime);
-      $('.time-block').each(function () {
-        var timeId = $(this).attr('id');
-        console.log(currentTime > timeId); 
-        if (currentTime > timeId) {
-        $(this).addClass('past');
-       } else if (currentTime < timeId) {
-        $(this).addClass('future').removeClass('past present');
-       } else { 
-        $(this).addClass('present').removeClass('past future');
-       }
       });
-    }
-
-
-
-
-
-
-
-
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-
-  // use the id in the containing time-block as a key to save the user input in
-
-  // local storage. HINT: What does `this` reference in the click listener
-
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-
-  // time-block containing the button that was clicked? How might the id be
-
-  // useful when saving the description in local storage?
-  //
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
+     
 });
+
+  
+
+var currentTime = dayjs().format("dddd MMMM DD");
+$("#currentDay").text(currentTime);
+
+
+for (var i = 9; i < 17; i++) {
+  var thisHr = localStorage.getItem('hour-' + i)
+  if (thisHr) {
+    console.log(thisHr);
+  }
+    
+}
+
+
+
+function setColors() {
+  //var currentTime = hour();
+  console.log(currentTime);
+  $(".time-block").each(function () {
+    var timeId = $(this).attr("id");
+    console.log(currentTime > timeId);
+    if (currentTime > timeId) {
+      $(this).addClass("past");
+    } else if (currentTime < timeId) {
+      $(this).addClass("future").removeClass("past present");
+    } else {
+      $(this).addClass("present").removeClass("past future");
+    }
+  });
+}
+
+
+function userSched() {
+  var s1 = localStorage.getItem("1");
+  $("#hour-9").children(".description").text(s1);
+
+  var s2 = localStorage.getItem("2");
+  $("#hour-10").children(".description").text(s2);
+
+  var s3 = localStorage.getItem("3");
+  $("#hour-11").children(".description").text(s3);
+
+  var s4 = localStorage.getItem("4");
+  $("#hour-12").children(".description").text(s4);
+
+  var s5 = localStorage.getItem("5");
+  $("#hour-13").children(".description").text(s5);
+
+  var s6 = localStorage.getItem("6");
+  $("#hour-14").children(".description").text(s6);
+
+  var s7 = localStorage.getItem("7");
+  $("#hour-15").children(".description").text(s7);
+
+  var s8 = localStorage.getItem("8");
+  $("#hour-16").children(".description").text(s8);
+
+  var s9 = localStorage.getItem("9");
+  $("#hour-17").children(".description").text(s9);
+}
+
+setColors();
+userSched();
